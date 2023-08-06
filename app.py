05679d6b2a0500d3e85542749a92863a1f2398bf
Justin Pyron <justinpyron@gmail.com>
2023-08-06 21:40:04 +0200

    Initialize streamlit app

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title='Bayesian Opt',
+    page_icon='🥇',
+    layout='wide',
+)
+st.title('Bayesian Optimization 🥇')
+st.write('👉 Learn how it works on [GitHub](https://github.com/justinpyron/bayesian-optimization)')
+
+# @st.cache_data
+# def initialize_ranker():
+#     return FootballRank()
+
+
+
