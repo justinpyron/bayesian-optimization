b1c47d8018d5ac7c91d7c88082ecc7cc1e91aa00
justinpyron <32315307+justinpyron@users.noreply.github.com>
2025-12-17 20:18:24 -0600

    Cloud run deployments (#5)
    * Add 'dev' dependency group
    
    * Add pre-commit
    
    * Lint
    
    * Add Dockerfile
    
    * Add GitHub Actions workflow

--- bayesian_optimization.py
+++ bayesian_optimization.py
@@ -1,15 +1,15 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from scipy.stats import norm
 from typing import Callable
+
+import numpy as np
+from scipy.stats import norm
 
 
 class BayesianOptimizer:
-    '''
+    """
     Class for performing Bayesian Optimization.
     See https://arxiv.org/abs/1807.02811 for theory details.
-    '''
+    """
+
     def __init__(
         self,
         function: Callable[[np.array], float],
@@ -24,56 +24,54 @@ class BayesianOptimizer:
         self.sample_points = self.uniformly_sample_domain(number_of_samples=1)
         self.sample_values = np.array([self.function(x) for x in self.sample_points])
 
-
     def uniformly_sample_domain(
-            self,
-            number_of_samples: int,
-        ) -> np.array:
-        return np.array([
-            np.random.uniform(lower, upper, number_of_samples)
-            for (lower, upper) in self.domain_bounds
-        ]).T
-
+        self,
+        number_of_samples: int,
+    ) -> np.array:
+        return np.array(
+            [
+                np.random.uniform(lower, upper, number_of_samples)
+                for (lower, upper) in self.domain_bounds
+            ]
+        ).T
 
     def compute_sample_variance(self) -> float:
-        '''Compute the variance of function values observed thus far'''
+        """Compute the variance of function values observed thus far"""
         return self.sample_values.var() if len(self.sample_values) > 1 else 1
-
 
     def kernel(
         self,
         x: np.array,
         y: np.array,
     ) -> float:
-        '''
+        """
         Return the covariance between two points.
 
         Parameter self.kernel_bandwidth controls how high covariance
         is as a function of distance between two points.
         Higher values of self.kernel_bandwidth lead to higher covariance.
-        
-        Returns the variance of self.function at a 
+
+        Returns the variance of self.function at a
         single point p when fed that point twice,
         i.e. kernel(p, p).
 
         Since the true variance of the underlying data
         distribution is unknown, it is estimated with
-        the variance of samples that have been collected 
+        the variance of samples that have been collected
         so far. There isn't a theoretical justification;
         it's simply seemed to me the most data-driven and
         and subjective estimation method.
-        '''
+        """
         return self.compute_sample_variance() * np.exp(
-            -0.5 * np.square(x - y).sum() / (self.kernel_bandwidth ** 2)
+            -0.5 * np.square(x - y).sum() / (self.kernel_bandwidth**2)
         )
-
 
     def compute_posterior(
         self,
         sample_point: np.array,
         step: int = None,
     ) -> tuple[np.array, np.array]:
-        '''
+        """
         Compute the mean and variance of a sample point, conditional on samples seen thus far.
 
         If [x_1, x_2] ~ N(mean, var),
@@ -84,52 +82,48 @@ class BayesianOptimizer:
         where
             mean_tilde = mean_1 + var_1_2 (var_2_2 ^ -1) (observed_values - mean_2)
             var_tilde  = var_1_1 - var_1_2 (var_2_2 ^ -1) var_2_1
-        
+
         In our setting,
             x_1 = sample_point
             x_2 = observed_values
-        '''
+        """
         observed_samples = self.sample_points[:step]
         observed_values = self.sample_values[:step]
         mean_1 = observed_values.mean()
         mean_2 = observed_values.mean() * np.ones_like(observed_values)
         var_1_1 = self.kernel(sample_point, sample_point)
-        var_1_2 = var_2_1 = np.array([
-            self.kernel(sample_point, x) for x in observed_samples
-        ])
-        var_2_2 = np.array([
-            [self.kernel(x, y) for x in observed_samples] for y in observed_samples
-        ])
+        var_1_2 = var_2_1 = np.array(
+            [self.kernel(sample_point, x) for x in observed_samples]
+        )
+        var_2_2 = np.array(
+            [[self.kernel(x, y) for x in observed_samples] for y in observed_samples]
+        )
         mean_tilde = mean_1 + var_1_2.dot(
             np.linalg.solve(var_2_2, observed_values - mean_2)
         )
-        var_tilde = var_1_1 - var_1_2.dot(
-            np.linalg.solve(var_2_2, var_2_1)
-        )
+        var_tilde = var_1_1 - var_1_2.dot(np.linalg.solve(var_2_2, var_2_1))
         return mean_tilde, var_tilde + 1e-5  # Add small value for numerical stability
-
 
     def compute_expected_improvement(
         self,
         point: np.array,
         step: int = None,
     ) -> float:
-        '''Compute expected improvement over highest observed point'''
+        """Compute expected improvement over highest observed point"""
         best_yet = self.sample_values[:step].max()
         mean, variance = self.compute_posterior(point, step)
         st_dev = np.sqrt(variance)
         if st_dev > 1e-5:
             Z = (mean - best_yet) / st_dev
-            return (mean - best_yet)*norm.cdf(Z) + st_dev*norm.pdf(Z)
+            return (mean - best_yet) * norm.cdf(Z) + st_dev * norm.pdf(Z)
         else:
             return np.maximum(0, mean - best_yet)
 
-
     def take_step(self) -> None:
-        '''Take a sample at the point that maximizes Expected Improvement'''
-        expected_improvement = np.array([
-            self.compute_expected_improvement(z) for z in self.domain_samples
-        ])
+        """Take a sample at the point that maximizes Expected Improvement"""
+        expected_improvement = np.array(
+            [self.compute_expected_improvement(z) for z in self.domain_samples]
+        )
         next_sample = self.domain_samples[np.argmax(expected_improvement)]
         self.sample_points = np.append(
             self.sample_points,
--- make_plot.py
+++ make_plot.py
@@ -1,6 +1,7 @@
 import numpy as np
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+
 from bayesian_optimization import BayesianOptimizer
 
 
@@ -10,121 +11,149 @@ def make_plot(
 ) -> go.Figure:
     domain = np.sort(optimizer.domain_samples, axis=0)
     true_function = np.array([optimizer.function(z) for z in domain])
-    expected_improvement = np.array([
-        optimizer.compute_expected_improvement(z, step_to_display) for z in domain
-    ])
+    expected_improvement = np.array(
+        [optimizer.compute_expected_improvement(z, step_to_display) for z in domain]
+    )
     posterior = [optimizer.compute_posterior(z, step_to_display) for z in domain]
     posterior_mean = np.array([_mean for (_mean, _var) in posterior])
     posterior_var = np.array([_var for (_mean, _var) in posterior])
-    lower_bound = posterior_mean - 1.96*np.sqrt(posterior_var)
-    upper_bound = posterior_mean + 1.96*np.sqrt(posterior_var)
+    lower_bound = posterior_mean - 1.96 * np.sqrt(posterior_var)
+    upper_bound = posterior_mean + 1.96 * np.sqrt(posterior_var)
 
     # Make figure
     fig = make_subplots(
         rows=2,
         cols=1,
-        row_heights=[3,1],
+        row_heights=[3, 1],
         shared_xaxes=True,
-        vertical_spacing = 0,
+        vertical_spacing=0,
     )
     fig.update_layout(
         height=500,
         margin=dict(l=0, r=0, t=0, b=0),
         legend=dict(
-            orientation='h',
-            xanchor='center',
+            orientation="h",
+            xanchor="center",
             x=0.5,
         ),
         xaxis_range=[
             optimizer.domain_bounds[0][0],
             optimizer.domain_bounds[0][1],
-        ]
+        ],
     )
-    fig.update_yaxes(range=[
-        true_function.min() - 0.5*(true_function.max() - true_function.min()),
-        true_function.max() + 0.5*(true_function.max() - true_function.min()),
-    ], row=1, col=1)
+    fig.update_yaxes(
+        range=[
+            true_function.min() - 0.5 * (true_function.max() - true_function.min()),
+            true_function.max() + 0.5 * (true_function.max() - true_function.min()),
+        ],
+        row=1,
+        col=1,
+    )
 
     # Bottom chart
-    fig.append_trace(go.Scatter(
-        x=domain[:,0],
-        y=expected_improvement,
-        name='Expected Improvement',
-        line=dict(
-            color='springgreen',
-            width=3,
+    fig.append_trace(
+        go.Scatter(
+            x=domain[:, 0],
+            y=expected_improvement,
+            name="Expected Improvement",
+            line=dict(
+                color="springgreen",
+                width=3,
+            ),
+            hoverlabel=dict(namelength=-1),
         ),
-        hoverlabel=dict(namelength=-1),
-    ), row=2, col=1)
+        row=2,
+        col=1,
+    )
 
     # Top chart
-    fig.append_trace(go.Scatter(
-        name='Estimated Function Uncertainty Lower Bound',
-        x=domain[:,0],
-        y=lower_bound,
-        line=dict(
-            color='dodgerblue',
-            width=0.5,
-        ),
-        hoverlabel=dict(namelength=-1),
-        showlegend=False,
-    ), row=1, col=1)
-    fig.append_trace(go.Scatter(
-        name='Estimated Function Uncertainty Upper Bound',
-        x=domain[:,0],
-        y=upper_bound,
-        line=dict(
-            color='dodgerblue',
-            width=0.5,
-        ),
-        hoverlabel=dict(namelength=-1),
-        showlegend=False,
-        fill='tonexty',
-        fillcolor='rgba(30,144,255,0.2)', # dodgerblue with less opacity
-    ), row=1, col=1)
-    fig.append_trace(go.Scatter(
-        name='Estimated Function',
-        x=domain[:,0],
-        y=posterior_mean,
-        line=dict(
-            color='dodgerblue',
-            width=3,
-        ),
-        hoverlabel=dict(namelength=-1),
-    ), row=1, col=1)
-    fig.append_trace(go.Scatter(
-        name='True Function',
-        x=domain[:,0],
-        y=true_function,
-        line=dict(
-            color='Orange',
-            width=5,
-        ),
-        hoverlabel=dict(namelength=-1),
-    ), row=1, col=1)
-    fig.append_trace(go.Scatter(
-        name='Oberved Values (label = order of observation)',
-        x=optimizer.sample_points[:,0][:step_to_display],
-        y=optimizer.sample_values[:step_to_display],
-        text=np.arange(1,len(optimizer.sample_values)+1).astype(str),
-        mode='markers+text',
-        marker=dict(
-            color='Orange',
-            size=18,
+    fig.append_trace(
+        go.Scatter(
+            name="Estimated Function Uncertainty Lower Bound",
+            x=domain[:, 0],
+            y=lower_bound,
             line=dict(
-                color='DarkOrange',
-                # color='lightslategray',
-                width=1.5
-            )
+                color="dodgerblue",
+                width=0.5,
+            ),
+            hoverlabel=dict(namelength=-1),
+            showlegend=False,
         ),
-        hoverlabel=dict(namelength=-1),
-    ), row=1, col=1)
+        row=1,
+        col=1,
+    )
+    fig.append_trace(
+        go.Scatter(
+            name="Estimated Function Uncertainty Upper Bound",
+            x=domain[:, 0],
+            y=upper_bound,
+            line=dict(
+                color="dodgerblue",
+                width=0.5,
+            ),
+            hoverlabel=dict(namelength=-1),
+            showlegend=False,
+            fill="tonexty",
+            fillcolor="rgba(30,144,255,0.2)",  # dodgerblue with less opacity
+        ),
+        row=1,
+        col=1,
+    )
+    fig.append_trace(
+        go.Scatter(
+            name="Estimated Function",
+            x=domain[:, 0],
+            y=posterior_mean,
+            line=dict(
+                color="dodgerblue",
+                width=3,
+            ),
+            hoverlabel=dict(namelength=-1),
+        ),
+        row=1,
+        col=1,
+    )
+    fig.append_trace(
+        go.Scatter(
+            name="True Function",
+            x=domain[:, 0],
+            y=true_function,
+            line=dict(
+                color="Orange",
+                width=5,
+            ),
+            hoverlabel=dict(namelength=-1),
+        ),
+        row=1,
+        col=1,
+    )
+    fig.append_trace(
+        go.Scatter(
+            name="Oberved Values (label = order of observation)",
+            x=optimizer.sample_points[:, 0][:step_to_display],
+            y=optimizer.sample_values[:step_to_display],
+            text=np.arange(1, len(optimizer.sample_values) + 1).astype(str),
+            mode="markers+text",
+            marker=dict(
+                color="Orange",
+                size=18,
+                line=dict(
+                    color="DarkOrange",
+                    # color='lightslategray',
+                    width=1.5,
+                ),
+            ),
+            hoverlabel=dict(namelength=-1),
+        ),
+        row=1,
+        col=1,
+    )
 
     fig.add_vline(
         x=domain[np.argmax(expected_improvement)][0],
         line_width=3,
-        line_dash='dash',
-        line_color='springgreen',
+        line_dash="dash",
+        line_color="springgreen",
     )
 
     return fig
